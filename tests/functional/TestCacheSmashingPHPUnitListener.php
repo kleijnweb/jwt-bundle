a6f5e782367bb5b0b66f8af2b3f6656f0e5a2e7a
John Kleijn <john@kleijnweb.nl>
2017-03-31 16:57:27 +0200

    Smash cache for real now

---
+++ tests/functional/TestCacheSmashingPHPUnitListener.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * This file is part of the KleijnWeb\JwtBundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KleijnWeb\JwtBundle\Tests\Functional;
+
+use PHPUnit_Framework_Test;
+use PHPUnit_Framework_AssertionFailedError;
+use Exception;
+use PHPUnit_Framework_TestSuite;
+
+class TestCacheSmashingPHPUnitListener implements \PHPUnit_Framework_TestListener
+{
+    const SUITE_NAME = 'Functional';
+
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        //NOOP
+    }
+
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        //NOOP
+    }
+
+    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        //NOOP
+    }
+
+    public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        //NOOP
+    }
+
+    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        //NOOP
+    }
+
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        //NOOP
+    }
+
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        //NOOP
+    }
+
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->smashIfFunctionalSuite($suite);
+    }
+
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->smashIfFunctionalSuite($suite);
+    }
+
+    private function smashIfFunctionalSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        if ($suite->getName() !== self::SUITE_NAME) {
+            return;
+        }
+
+        $dir = __DIR__ . '/App/app/cache';
+
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $path) {
+            $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
+        }
+    }
+}
